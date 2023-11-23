9a4f2009df117146a53adf78b9fc6f76b924eab1
Baracho <baracholss@gmail.com>
2023-11-23 10:32:08 -0300

    iniciando projeto do gateway

---
+++ mscloudgatway/src/main/java/com/baracho/mscloudgatway/MscloudgatwayApplication.java
@@ -0,0 +1,13 @@
+package com.baracho.mscloudgatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscloudgatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudgatwayApplication.class, args);
+	}
+
+}
