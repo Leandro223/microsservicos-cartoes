f42a44bbc9363854ef34b672f9e58383b1a776ca
Baracho <baracholss@gmail.com>
2023-11-23 14:26:03 -0300

    iniciando projeto micro serviço de cartão

---
+++ mscartao/src/test/java/com/baracho/mscartao/MscartaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baracho.mscartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscartaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
