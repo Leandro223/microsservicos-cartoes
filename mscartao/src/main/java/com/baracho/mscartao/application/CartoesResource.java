19d2c7af5800e9d6bade97198a2cd7a76916ef23
Baracho <baracholss@gmail.com>
2023-11-23 15:31:35 -0300

    correção anotação GetMapping

--- mscartao/src/main/java/com/baracho/mscartao/application/CartoesResource.java
+++ mscartao/src/main/java/com/baracho/mscartao/application/CartoesResource.java
@@ -1,6 +1,7 @@
 package com.baracho.mscartao.application;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CartoesResource {
 
+      @GetMapping
       public String status() {
             return "ok";
       }
